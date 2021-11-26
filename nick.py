a2d75bd52982b3b13e6f842f5b9065cc2338b4fa
Anthony Bechard <code@anthonybechard.me>
2021-11-26 18:32:08 -0500

    Added comments

--- nick.py
+++ nick.py
@@ -2,6 +2,9 @@ from datetime import datetime, timedelta
 
 
 def untilnick():
+    ''' This function calculates how many minutes are left before Nick is back at work
+    :return: return the amount of minutes before Nick is back to work
+    '''
     montrealtime = datetime.now()
     if isnickworkingtoday():
         delta = timedelta(days=-1, hours=(29 - montrealtime.hour), minutes=(60 - montrealtime.minute))
@@ -14,6 +17,9 @@ def untilnick():
 
 
 def isnickworking():
+    ''' This function checks if Nick is currently working
+    :return: True if Nick is working, False if not isn't working
+    '''
     montrealtime = datetime.now()
     if montrealtime.weekday() <= 4 and (
             (6 <= montrealtime.hour < 14) or (montrealtime.hour == 14 and montrealtime.minute <= 30)):
@@ -23,6 +29,9 @@ def isnickworking():
 
 
 def isnickworkingtoday():
+    ''' This function checks if Nick is working today
+    :return: True if Nick is working today, False if not isn't working today
+    '''
     montrealtime = datetime.now()
     if montrealtime.weekday() <= 4 and (montrealtime.hour < 14 or (montrealtime.hour == 14 and montrealtime.minute <= 30)):
         return True
@@ -31,13 +40,12 @@ def isnickworkingtoday():
 
 
 def checkfornick():
+    ''' This function checks the time before Nick is back at work
+    :return: The numbers of minutes before Nick works, otherwise returns -1 if Nick is currently working
+    '''
     if not isnickworking():
-        #print('Nick will be back in {} minute(s)'.format(untilnick()))
-        #return 'Nick will be back in {} minute(s)'.format(untilnick())
         return untilnick()
     else:
-        #print('Nick is working right now!')
-        #return 'Nick is working right now!'
         return -1
 
 
